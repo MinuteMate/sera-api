9cc94f9cd6d235d9f774749268ab870fa1b71bec
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-05 01:26:22 +0900

    Initial commit

---
+++ src/main/java/com/minutemate/sera_api/SeraApiApplication.java
@@ -0,0 +1,13 @@
+package com.minutemate.sera_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeraApiApplication.class, args);
+    }
+
+}
